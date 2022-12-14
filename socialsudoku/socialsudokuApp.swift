fec1df6708217040815ca731966f195bd3654090
thomastopuz <thomatop@gmail.com>
2022-12-14 09:17:31 +0100

    Initial Commit

---
+++ socialsudoku/socialsudokuApp.swift
@@ -0,0 +1,17 @@
+//
+//  socialsudokuApp.swift
+//  socialsudoku
+//
+//  Created by Thomas Topuz on 14.12.22.
+//
+
+import SwiftUI
+
+@main
+struct socialsudokuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
